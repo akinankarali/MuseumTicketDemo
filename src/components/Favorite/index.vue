3cd0632e1875dd5cab2f1eae1fd66236447bfb16
KARIYER\saim.ankarali <saim.ankarali@kariyer.net>
2021-09-13 17:15:27 +0300

    feat: created Favorite button component

---
+++ src/components/Favorite/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="favorite">
+    <button>
+      <IconWishlist />
+    </button>
+  </div>
+</template>
+
+<script>
+import IconWishlist from '@/assets/svg/wishlist.svg'
+export default {
+  name: 'Favorite',
+  components: {
+    IconWishlist
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.favorite {
+  button {
+    border: 1px solid #{'rgb(var(--secondary))'};
+    padding: 6px;
+    border-radius: 999px;
+    display: inline-flex;
+    transition: border 0.5s;
+
+    svg path {
+      fill: #{'rgb(var(--secondary))'};
+      transition: fill 0.5s;
+    }
+    &:hover {
+      border: 1px solid #{'rgb(var(--primary))'};
+
+      svg path {
+        fill: #{'rgb(var(--primary))'} !important;
+      }
+    }
+  }
+}
+</style>
