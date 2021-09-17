1e1411af8487427c491c6e507cc3bdb922b60893
KARIYER\saim.ankarali <saim.ankarali@kariyer.net>
2021-09-18 01:36:55 +0300

    refactor: updated with mock data and gives props to component

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
-    <Card title="AAA" description="BBB" price="130" />
+    <div :key="index" v-for="(item, index) in list">
+      <Card
+        :title="item.title"
+        :description="item.description"
+        :price="item.price"
+        :discountedPrice="item.discountedPrice"
+        :isPriceDiscounted="item.isPriceDiscounted"
+        :isInCart="item.isInCart"
+      />
+    </div>
   </div>
 </template>
 
@@ -11,6 +20,44 @@ export default {
   name: 'Home',
   components: {
     Card
+  },
+  data() {
+    return {
+      list: [
+        {
+          title: 'PRODUCT TITLE-1',
+          description: 'Torino',
+          price: 80,
+          discountedPrice: 50,
+          isPriceDiscounted: true,
+          isInCart: false
+        },
+        {
+          title: 'PRODUCT TITLE-2',
+          description: 'Milan',
+          price: 130,
+          discountedPrice: 0,
+          isPriceDiscounted: false,
+          isInCart: true
+        },
+        {
+          title: 'PRODUCT TITLE-3',
+          description: 'Cambridge',
+          price: 250,
+          discountedPrice: 200,
+          isPriceDiscounted: true,
+          isInCart: false
+        },
+        {
+          title: 'PRODUCT TITLE-4',
+          description: 'Amsterdam',
+          price: 100,
+          discountedPrice: 0,
+          isPriceDiscounted: false,
+          isInCart: false
+        }
+      ]
+    }
   }
 }
 </script>
@@ -18,5 +65,10 @@ export default {
 <style lang="scss" scope>
 .home {
   padding: 10px;
+
+  display: flex;
+  flex: 0 1 auto;
+  flex-flow: row wrap;
+  margin: 0 -10px;
 }
 </style>
