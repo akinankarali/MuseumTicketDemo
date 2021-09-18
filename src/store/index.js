4472561d2a1a524b21d0455555ccb67f40faeefa
KARIYER\saim.ankarali <saim.ankarali@kariyer.net>
2021-09-18 19:17:54 +0300

    feat: create store and call api with axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    products: []
+  },
+  getters: {
+    getProducts(state) {
+      return state.products
+    }
+  },
+  mutations: {
+    updateProducts(state, payload) {
+      state.products = payload
+    },
+    setProducts(state, data) {
+      state.products = data
+    }
+  },
+  actions: {
+    async fetchProducts({ commit }) {
+      const data = await axios.get('https://sandbox.musement.com/api/v3/events')
+      commit('setProducts', data)
+    }
+  }
 })
