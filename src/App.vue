5016440d3e32a56c4cfe32e0f8788fa84b3b1dfb
KARIYER\saim.ankarali <saim.ankarali@kariyer.net>
2021-09-19 23:41:05 +0300

    feat: added footer

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="app">
-    <MasterHeader />
     <Container>
+      <MasterHeader />
       <main class="main">
         <router-view />
       </main>
+      <footer>
+        <CustomText tag="p">Footer</CustomText>
+      </footer>
     </Container>
   </div>
 </template>
 <script>
 import MasterHeader from '@/components/Header'
 import Container from '@/components/Container'
+import CustomText from '@/components/CustomText'
 export default {
   name: 'App',
   components: {
     MasterHeader,
-    Container
+    Container,
+    CustomText
   }
 }
 </script>
+<style lang="scss" scoped>
+footer {
+  p {
+    font-family: 'Lato-Bold', sans-serif;
+    letter-spacing: 1.37px;
+    padding: 0 40px;
+  }
+}
+</style>
